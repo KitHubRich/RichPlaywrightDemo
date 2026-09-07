78058c604aa4f44e681d7ab11023c025d361df08
KitHubRich <148050597+KitHubRich@users.noreply.github.com>
2026-09-07 18:42:42 +0100

    Refactor BBC homepage test for clarity
    Refactor test to improve readability and functionality.

--- tests/bbc.spec.ts
+++ tests/bbc.spec.ts
@@ -3,15 +3,11 @@ import { test, expect } from '@playwright/test';
 test('BBC homepage has BBC in title and opens news', async ({ page }) => {
 await page.goto('https://www.bbc.co.uk');
 
-await page.getByTestId('accept-button').click();
+// await page.getByTestId('accept-button').click();
 
 await expect(page).toHaveTitle(/BBC/);
 
-await page
-.getByTestId('header-content')
-.getByRole('link', { name: 'News' })
-.click();
+await page.getByRole('link', { name: 'News' }).first().click();
 
 await expect(page).toHaveURL(/news/);
-await page.waitForTimeout(5000);
 });
