b7d49cfd84ba9397b0ac742d92cda3b2dfd95695
KitHubRich <richjs80100@gmail.com>
2026-09-08 10:55:40 +0100

    Add BBC News heading validation

--- tests/bbc.spec.ts
+++ tests/bbc.spec.ts
@@ -1,13 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('BBC homepage opens News successfully', async ({ page }) => {
-await page.goto('https://www.bbc.co.uk');
+  await page.goto('https://www.bbc.co.uk');
 
-// await page.getByTestId('accept-button').click();
+  // await page.getByTestId('accept-button').click();
 
-await expect(page).toHaveTitle(/BBC/);
+  await expect(page).toHaveTitle(/BBC/);
 
-await page.getByRole('link', { name: 'News' }).first().click();
+  await page.getByRole('link', { name: 'News' }).first().click();
 
-await expect(page).toHaveURL(/news/);
+  await expect(page).toHaveURL(/news/);
+
+  await expect(
+    page.getByRole('heading', { name: 'BBC News', exact: true })
+  ).toBeVisible();
 });
