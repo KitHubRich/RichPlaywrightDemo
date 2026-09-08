b489dc0dd28e8ea37f7a4f218569410acd11125a
KitHubRich <richjs80100@gmail.com>
2026-09-08 17:34:01 +0100

    Simplify BBC link validation test

--- tests/bbc-newsarticle.spec.ts
+++ tests/bbc-newsarticle.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('User can accept cookies and open England news', async ({ page }) => {
+test('BBC homepage displays News and Sport links', async ({ page }) => {
   await page.goto('https://www.bbc.co.uk/');
 
   const acceptButton = page.getByTestId('accept-button');
@@ -9,17 +9,11 @@ test('User can accept cookies and open England news', async ({ page }) => {
     await acceptButton.click();
   }
 
-  await page.getByRole('link', { name: 'News' }).first().click();
+  await expect(
+    page.getByRole('link', { name: 'News' }).first()
+  ).toBeVisible();
 
-  await expect(page).toHaveURL(/news/);
-
-  const englandLink = page.locator('a[href="/news/england"]').first();
-
-  await expect(englandLink).toBeVisible();
-
-  await englandLink.click();
-
-  await expect(page).toHaveURL(/england/i);
-
-  await expect(page.getByRole('heading').first()).toBeVisible();
+  await expect(
+    page.getByRole('link', { name: 'Sport' }).first()
+  ).toBeVisible();
 });
